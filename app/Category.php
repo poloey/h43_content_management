7da126525d1d35c4fd8087aaff17b5d973b5615e
polo dev <polodev10@gmail.com>
2018-01-18 12:47:19 +0600

    added category and post relationship

--- app/Category.php
+++ app/Category.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App;
+use App\Post;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
   protected $guarded = [];
+  public function posts () {
+    return $this->hasMany(Post::class);
+  }
 }
--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,7 @@ namespace App;
 
 use App\Comment;
 use App\User;
+use App\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -11,6 +12,9 @@ class Post extends Model
   protected $guarded = [];
   public function user() {
     return $this->belongsTo(User::class);
+  }
+  public function category() {
+    return $this->belongsTo(Category::class);
   }
   public function comments () {
     return $this->hasMany(Comment::class);
